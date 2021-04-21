1c004e5870b21f9093f8598f7b43fb3ad653ff7b
Pierre Houzelstein <pierrehouzelstein@pierrembp.home>
2021-04-21 16:20:40 +0200

    Added function to fit the decay of the amplitude and find the period

--- Heun_method.py
+++ Heun_method.py
@@ -9,14 +9,15 @@ from numpy import linspace, array, meshgrid, zeros, ndarray, shape
 from numpy import sin, cos, pi, random, average, exp, inf, sqrt, diag
 from datetime import datetime
 import matplotlib.pyplot as plt
-from numba import jit, float64, int64
+from matplotlib.axis import Axis
+from numba import jit, float64, int64, types
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 from tqdm import tqdm
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
-@jit
+@jit((float64[:])(float64[:], float64, float64, float64),nopython=True)
 def stuartLandau(z, a, alpha, mu):
     """
     Studied function
@@ -26,6 +27,7 @@ def stuartLandau(z, a, alpha, mu):
     fy = alpha*y*(mu - (x**2 + y**2)) + x*(1 + alpha*a*(x**2 + y**2))
     return array([fx, fy])
 
+@jit((float64[:])(float64[:]),nopython=True)
 def noiseFunction(z):
     """
     Added noise
@@ -35,6 +37,7 @@ def noiseFunction(z):
     gy = 0.1
     return array([gx, gy])
 
+@jit
 def vector_space(a, alpha, mu):
     """
     Plots the vector field linked to the system
@@ -47,6 +50,7 @@ def vector_space(a, alpha, mu):
     plt.ylabel('y')
     plt.show()
 
+@jit((float64[:])(float64[:], float64, float64, float64, float64, float64),nopython=True)
 def predictor(z, h, eta, a, alpha, mu):
     """
     Computes predictor for Euler integration
@@ -54,6 +58,7 @@ def predictor(z, h, eta, a, alpha, mu):
     pred = z + h*stuartLandau(z, a, alpha, mu) + eta*noiseFunction(z)
     return pred
 
+@jit((float64[:])(float64[:], float64, float64, float64, float64, float64),nopython=True)
 def update_z(z, h, eta, a, alpha, mu):
     """
     Gives z at t + h from z at t
@@ -62,6 +67,7 @@ def update_z(z, h, eta, a, alpha, mu):
     z_updated = z + 0.5*(stuartLandau(z, a, alpha, mu) + stuartLandau(pred, a, alpha, mu))*h + 0.5*(noiseFunction(z) + noiseFunction(pred))*eta
     return z_updated
 
+@jit(types.Tuple((float64[:], float64[:,:]))(float64, float64, float64, int64, float64, float64),nopython=True)
 def EulerInteg(h, a, alpha, numsteps, phase, mu):
     """
     Integration routine
@@ -72,12 +78,12 @@ def EulerInteg(h, a, alpha, numsteps, phase, mu):
     
     #Setting up lists
     time_list = zeros(numsteps+1)
-    trajectory = zeros((numsteps+1, 2), dtype=ndarray)
+    trajectory = zeros((numsteps+1, 2))#, dtype=ndarray)
     time_list[0] = time
     trajectory[0] = z
     
     for i in range(numsteps):
-        eta = random.normal(loc=0, scale=sqrt(h), size=1)[0], random.normal(loc=0, scale=sqrt(h), size=1)[0]
+        eta = array(random.normal(loc=0, scale=sqrt(h), size=1)[0], random.normal(loc=0, scale=sqrt(h), size=1)[0])
 
         z_updated = update_z(z, h, eta, a, alpha, mu)
         
@@ -86,9 +92,9 @@ def EulerInteg(h, a, alpha, numsteps, phase, mu):
         # i+1 because 0 is set at initial conditions
         time_list[i+1] = time
         trajectory[i+1] = z
-        
     return time_list, trajectory
 
+@jit(types.Tuple((float64[:], float64[:,:]))(float64, float64, float64, int64, int64, float64, float64),nopython=True)
 def stochastic_average(h, a, alpha, numsteps, numiter, phase, mu):
     """
     The main piece
@@ -97,9 +103,6 @@ def stochastic_average(h, a, alpha, numsteps, numiter, phase, mu):
     #Same times for all trajectories
     times = zeros(numsteps+1)
     trajectories = zeros((numiter, numsteps+1, 2), dtype=ndarray)
-    
-    print("Computing the Euler integration (this might take a while)...")
-    
     
     for i in tqdm(range(numiter)):
         times, trajectories[i] = EulerInteg(h, a, alpha, numsteps, phase, mu)
@@ -114,19 +117,23 @@ def stochastic_average(h, a, alpha, numsteps, numiter, phase, mu):
     return times, stochastic_av
 
 #exponential function for fitting
+@jit
 def exponential(x, a, b):
     return a * exp( -b * x)
 
-def find_amplitudes(time, trajectory):
+@jit
+def find_amplitudes(time, trajectory, sensitivity):
     """
     Find indexes of local maxima, their value, and the time t associated
     """
     #Select points in time and trajectory lists corresponding to maxima
-    peaks, properties = find_peaks(trajectory, height = 0.6, prominence=1)
+
+    peaks, properties = find_peaks(trajectory, height = 0, prominence=sensitivity)
+
     times = time[peaks]
     amplitudes = trajectory[peaks]
     
-    period_list = zeros(len(peaks)-1)
+    period_list = zeros(len(peaks) - 1)
     for i in range(len(peaks)-1):
         period = times[i+1] - times[i]
         period_list[i] = period
@@ -139,11 +146,12 @@ def find_amplitudes(time, trajectory):
     popt, pcov = curve_fit(exponential, times, amplitudes)
     
     return times, amplitudes, period, popt
-        
+
 def main():
     startTime = datetime.now()
 
-    h=0.01; a, alpha = [0, 1]; numsteps = 10000; numiter = 1000; start_phase = random.uniform(-1, 1); mu = 1
+    h=0.01; a, alpha = [0.0, 1.0]; numsteps = 10000; numiter = 100; start_phase = random.uniform(-1, 1); mu = 1.0
+    sensitivity = 1. #parameter used to find the peaks; around 1 when limit cycle
 
     #Example of integration
     time_list, trajectory = EulerInteg(h, a, alpha, numsteps,  start_phase, mu)
@@ -156,14 +164,14 @@ def main():
     plt.xlabel("t")
     plt.ylabel("x")
     plt.show()
-
+    
     times, stochastic_av = stochastic_average(h, a, alpha, numsteps, numiter, start_phase, mu)
     
     plt.plot(stochastic_av[:,0], stochastic_av[:,1], color='blue')
     plt.title("Average trajectory with noise")
     vector_space(a, alpha, mu)
     
-    times_maxima, maxima, period, params = find_amplitudes(times,stochastic_av[:,0])
+    times_maxima, maxima, period, params = find_amplitudes(times, stochastic_av[:,0], sensitivity) #error there
     
     plt.plot(times, stochastic_av[:,0], color='blue')
     #Check graphically if the maxima have been found
@@ -172,19 +180,20 @@ def main():
     plt.xlabel("t")
     plt.ylabel("x")
     plt.show()
-    
+
     #Compare with numerical values found
     plt.plot(times_maxima, maxima, "r+")
     t = linspace(0, h*numsteps, num=numsteps)
-    plt.plot(t, exponential(t, params[0], params[1]))
+    plt.plot(t, exponential(t, params[0], params[1]), color='green')
     plt.title("Average oscillations with noise")
+    plt.legend(["Peaks", "Exponential fit"])
     plt.xlabel("t")
     plt.ylabel("x")
     plt.show()
 
     print("The average period of the oscillations is", period, "s")
-    print('The rate of decay is', params[1], "s" + "\u00B2")
-    
+    print('The rate of decay is', params[1], "s-1")
+
     print('\tiniTime: %s\n\tendTime: %s' % (startTime, datetime.now()))
 
 if __name__ == '__main__':
